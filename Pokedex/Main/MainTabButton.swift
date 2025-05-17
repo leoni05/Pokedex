9495dbd4d83a43f3bded597754bc9ba84c334c0e
leoni05 <yongjun18976@gmail.com>
2025-05-17 23:47:57 +0900

    main tab button 세팅

---
+++ Pokedex/Main/MainTabButton.swift
@@ -0,0 +1,53 @@
+//
+//  MainTabButton.swift
+//  Pokedex
+//
+//  Created by leoni05 on 5/17/25.
+//
+
+import Foundation
+import UIKit
+import PinLayout
+
+class MainTabButton: UIButton {
+    
+    // MARK: - Properties
+    
+    private let containerView = UIView()
+    private let btnImageView = UIImageView()
+    private let btnLabel = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    init(imageName: String, labelString: String) {
+        super.init(frame: .zero)
+        
+        btnLabel.text = labelString
+        btnLabel.textColor = .wineRed
+        btnLabel.font = .systemFont(ofSize: 14, weight: .heavy)
+        
+        btnImageView.image = UIImage(named: imageName)
+        btnImageView.contentMode = .scaleAspectFit
+        
+        commonInit()
+    }
+    
+    private func commonInit() {
+        addSubview(containerView)
+        containerView.addSubview(btnImageView)
+        containerView.addSubview(btnLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        btnImageView.pin.top().left().size(24)
+        btnLabel.pin.below(of: btnImageView, aligned: .center).marginTop(3).sizeToFit()
+        containerView.pin.center().wrapContent()
+    }
+    
+}
