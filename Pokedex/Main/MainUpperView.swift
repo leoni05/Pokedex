4fa55a8bf29042de1bb45d56f649667506c869f5
leoni05 <yongjun18976@gmail.com>
2025-05-17 12:01:16 +0900

    MainUpperView 셑팅

---
+++ Pokedex/Main/MainUpperView.swift
@@ -0,0 +1,13 @@
+//
+//  MainUpperView.swift
+//  Pokedex
+//
+//  Created by leoni05 on 5/17/25.
+//
+
+import Foundation
+import UIKit
+
+class MainUpperView: UIView {
+    
+}
--- Pokedex/Main/MainViewController.swift
+++ Pokedex/Main/MainViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import PinLayout
 
 class MainViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    private let mainUpperView = MainUpperView()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(mainUpperView)
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        mainUpperView.pin.top(self.view.pin.safeArea).horizontally(self.view.pin.safeArea)
+            .height(72)
+    }
 
 }
 
