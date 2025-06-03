af698a2cab6250239626d0449e57a6d9d499e8c6
leoni05 <yongjun18976@gmail.com>
2025-06-03 20:14:11 +0900

    탭 별 샘플 vc 세팅

---
+++ Pokedex/Camera/CameraViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CameraViewController.swift
+//  Pokedex
+//
+//  Created by leoni05 on 6/3/25.
+//
+
+import Foundation
+import UIKit
+import PinLayout
+
+class CameraViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    private let label = UILabel()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        label.text = "CameraViewController"
+        label.textColor = .wineRed
+        label.font = UIFont(name: "Galmuri11-Bold", size: 14)
+        self.view.addSubview(label)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        label.pin.center().sizeToFit()
+    }
+    
+}
