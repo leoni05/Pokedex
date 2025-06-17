b3c2f51a199872bee0da0e72d511fffab0a837b7
leoni05 <yongjun18976@gmail.com>
2025-06-17 21:04:09 +0900

    gotcha vc 세팅

--- Pokedex/Gotcha/GotchaViewController.swift
+++ Pokedex/Gotcha/GotchaViewController.swift
@@ -14,9 +14,47 @@ class GotchaViewController: UIViewController {
     
     // MARK: - Properties
     
-    
+    private var presentingVC: UIViewController? = nil
     
     // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if presentingVC == nil {
+            presentingVC = CameraViewController()
+            if let vc = presentingVC {
+                self.addChild(vc)
+                vc.view.frame = .zero
+                self.view.insertSubview(vc.view, at: 0)
+                vc.view.didMoveToSuperview()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let vc = presentingVC {
+            vc.view.pin.all()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if presentingVC is CameraViewController {
+            if let vc = presentingVC {
+                vc.willMove(toParent: nil)
+                vc.view.removeFromSuperview()
+                vc.removeFromParent()
+            }
+            presentingVC = nil
+        }
+    }
     
 }
 
@@ -24,6 +62,8 @@ class GotchaViewController: UIViewController {
 
 extension GotchaViewController {
     func takePicture() {
-        
+        if let cameraVC = presentingVC as? CameraViewController {
+            cameraVC.takePicture()
+        }
     }
 }
