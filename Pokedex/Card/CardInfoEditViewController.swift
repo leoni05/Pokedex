7ccd0fd6e2a97a3c6be13d831162298fc474e209
leoni05 <yongjun18976@gmail.com>
2025-07-26 00:42:23 +0900

    card info edit vc > ok button 세팅

--- Pokedex/Card/CardInfoEditViewController.swift
+++ Pokedex/Card/CardInfoEditViewController.swift
@@ -13,19 +13,35 @@ class CardInfoEditViewController: UIViewController {
     
     // MARK: - Properties
 
-    
+    private let okButton = UIButton()
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        okButton.layer.borderWidth = 2.0
+        okButton.layer.borderColor = UIColor.wineRed.cgColor
+        okButton.backgroundColor = .white
+        okButton.setTitle("확인", for: .normal)
+        okButton.setTitleColor(.wineRed, for: .normal)
+        okButton.titleLabel?.font = UIFont(name: "Galmuri11-Bold", size: 24)
+        okButton.addTarget(self, action: #selector(okButtonPressed(_:)), for: .touchUpInside)
+        self.view.addSubview(okButton)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        okButton.pin.bottom(16).right(16).width(68).height(52)
     }
     
 }
 
+// MARK: - Private Extensions
+
+private extension CardInfoEditViewController {
+    @objc func okButtonPressed(_ sender: UIButton) {
+        
+    }
+}
