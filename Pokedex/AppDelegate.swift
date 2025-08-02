4575e3d764362ab3f35f81af189ea1b822244d93
leoni05 <yongjun18976@gmail.com>
2025-08-02 14:13:08 +0900

    core data manager > persistent container 위치 변경

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -12,20 +12,9 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Model")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error {
-                fatalError("Unable to load persistent stores: \(error)")
-            }
-        })
-        return container
-    }()
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        CoreDataManager.shared.context = persistentContainer.viewContext
         return true
     }
 
--- Pokedex/CoreData/CoreDataManager.swift
+++ Pokedex/CoreData/CoreDataManager.swift
@@ -12,38 +12,44 @@ import CoreData
 class CoreDataManager {
     
     static let shared: CoreDataManager = CoreDataManager() 
-    var context: NSManagedObjectContext? = nil
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        })
+        return container
+    }()
     
     private init() { }
     
     func getPhotos() -> [Photo] {
         var photos: [Photo] = []
-        if let context = context {
-            let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Photo")
-            fetchRequest.sortDescriptors = [
-                NSSortDescriptor(key: "captureDate", ascending: false)
-            ]
-            do {
-                if let fetchResult = try context.fetch(fetchRequest) as? [Photo] {
-                    photos = fetchResult
-                }
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Photo")
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(key: "captureDate", ascending: false)
+        ]
+        do {
+            if let fetchResult = try persistentContainer.viewContext.fetch(fetchRequest) as? [Photo] {
+                photos = fetchResult
             }
-            catch {
-                print("Fetch failed: \(error)")
-            }
+        }
+        catch {
+            print("Fetch failed: \(error)")
         }
         return photos
     }
     
     func savePhoto(captureDate: Date, name: String, resultString: String, completion: @escaping () -> Void) {
-        if let context = context,
-           let entity = NSEntityDescription.entity(forEntityName: "Photo", in: context),
-           let photo = NSManagedObject(entity: entity, insertInto: context) as? Photo {
+        if let entity = NSEntityDescription.entity(forEntityName: "Photo", in: persistentContainer.viewContext),
+           let photo = NSManagedObject(entity: entity, insertInto: persistentContainer.viewContext) as? Photo {
             photo.captureDate = captureDate
             photo.name = name
             photo.resultString = resultString
             do {
-                try context.save()
+                try persistentContainer.viewContext.save()
                 completion()
             }
             catch {
