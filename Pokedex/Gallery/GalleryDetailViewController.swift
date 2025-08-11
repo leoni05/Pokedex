58ff6aea9fc4db9826f186a200855546e9f2f6e2
leoni05 <yongjun18976@gmail.com>
2025-08-11 22:41:53 +0900

    gallery detail vc > 스크롤 뷰, 이미지 뷰 세팅

--- Pokedex/Gallery/GalleryDetailViewController.swift
+++ Pokedex/Gallery/GalleryDetailViewController.swift
@@ -21,6 +21,7 @@ class GalleryDetailViewController: UIViewController {
     
     var imageName: String?
     
+    private let scrollView = UIScrollView()
     private let imageView = UIImageView()
     private let xpTitleLabel = UILabel()
     private let starsLabel = UILabel()
@@ -35,11 +36,27 @@ class GalleryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(scrollView)
+        
+        if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first,
+           let imageName = imageName {
+            let fileUrl = documentsDirectory.appendingPathComponent(imageName, conformingTo: .jpeg)
+            if FileManager.default.fileExists(atPath: fileUrl.path) {
+                imageView.image = UIImage(contentsOfFile: fileUrl.path)
+            }
+        }
+        imageView.contentMode = .scaleAspectFill
+        scrollView.addSubview(imageView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        scrollView.pin.all()
+        imageView.pin.top(MainUpperView.topInset + 32).horizontally().aspectRatio()
+        
+        scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: imageView.frame.maxY + 12)
     }
     
     override func viewDidAppear(_ animated: Bool) {
