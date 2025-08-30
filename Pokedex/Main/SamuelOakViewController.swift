5be1424d6d75d1712b3655877e27e3cd6958b138
leoni05 <yongjun18976@gmail.com>
2025-08-30 23:56:04 +0900

    samuel oak vc > 화면 터치 시 예/아니오 버튼 show

--- Pokedex/Main/SamuelOakViewController.swift
+++ Pokedex/Main/SamuelOakViewController.swift
@@ -20,6 +20,9 @@ class SamuelOakViewController: UIViewController {
     private let arrowLabel = UILabel()
     private var timerForBlink: Timer? = nil
     private let speechLabel = UILabel()
+    private let confirmBubbleView = SpeechBubbleView()
+    private let okButton = UIButton()
+    private let cancelButton = UIButton()
     
     // MARK: - Life Cycle
     
@@ -51,6 +54,19 @@ class SamuelOakViewController: UIViewController {
         arrowLabel.font = UIFont(name: "Galmuri11-Bold", size: 13)
         arrowLabelWrapper.addSubview(arrowLabel)
         
+        confirmBubbleView.isHidden = true
+        self.view.addSubview(confirmBubbleView)
+        
+        okButton.setTitle("예", for: .normal)
+        okButton.titleLabel?.font = UIFont(name: "Galmuri11-Regular", size: 16)
+        okButton.setTitleColor(.wineRed, for: .normal)
+        confirmBubbleView.addSubview(okButton)
+        
+        cancelButton.setTitle("아니오", for: .normal)
+        cancelButton.titleLabel?.font = UIFont(name: "Galmuri11-Regular", size: 16)
+        cancelButton.setTitleColor(.wineRed, for: .normal)
+        confirmBubbleView.addSubview(cancelButton)
+        
         setArrowLabel(hidden: false)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(screenPressed(_:)))
@@ -66,6 +82,10 @@ class SamuelOakViewController: UIViewController {
         arrowLabelWrapper.pin.vCenter(to: speechBubbleView.edge.bottom).right(to: speechBubbleView.edge.right)
             .size(18).marginRight(12).marginTop(-2)
         arrowLabel.pin.center().sizeToFit()
+        
+        okButton.pin.left().top().height(50).width(80)
+        cancelButton.pin.below(of: okButton, aligned: .left).height(50).width(80).marginBottom(4)
+        confirmBubbleView.pin.above(of: speechBubbleView, aligned: .right).wrapContent(padding: 4).marginBottom(8)
         
         samuelOakWrapperView.pin.above(of: speechBubbleView)
             .top(self.view.pin.safeArea).horizontally(self.view.pin.safeArea)
@@ -96,5 +116,7 @@ private extension SamuelOakViewController {
         self.setArrowLabel(hidden: true)
         speechLabel.text = "포켓몬스터 데이터를 다운로드 받을 준비는 되었는가?\n(Wi-Fi 환경 이용을 절대 권장합니다)"
         speechLabel.pin.top(14).horizontally(16).sizeToFit(.width)
+        
+        confirmBubbleView.isHidden = false
     }
 }
