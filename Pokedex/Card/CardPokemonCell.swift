12371217d818316aa5199526bb5da3c1c53d9215
leoni05 <yongjun18976@gmail.com>
2025-09-07 18:56:04 +0900

    card pokemon cell > 터치 이벤트 구현

--- Pokedex/Card/CardPokemonCell.swift
+++ Pokedex/Card/CardPokemonCell.swift
@@ -13,6 +13,7 @@ class CardPokemonCell: UIView {
     
     // MARK: - Properties
     
+    private let containerView = UIView()
     private let pokemonImageView = UIImageView()
     
     // MARK: - Life Cycle
@@ -20,13 +21,20 @@ class CardPokemonCell: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.layer.cornerRadius = 4.0
-        self.layer.borderColor = CGColor(red: 229.0/255.0, green: 229.0/255.0, blue: 229.0/255.0, alpha: 1.0)
-        self.layer.borderWidth = 1.0
+        containerView.layer.cornerRadius = 4.0
+        containerView.layer.borderColor = CGColor(red: 229.0/255.0, green: 229.0/255.0, blue: 229.0/255.0, alpha: 1.0)
+        containerView.layer.borderWidth = 1.0
+        self.addSubview(containerView)
         
         pokemonImageView.contentMode = .center
         pokemonImageView.image = UIImage(named: "pokeball.small")
-        self.addSubview(pokemonImageView)
+        containerView.addSubview(pokemonImageView)
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(cellLongPressed(_:)))
+        longPressGesture.minimumPressDuration = 0
+        longPressGesture.delegate = self
+        longPressGesture.cancelsTouchesInView = false
+        self.addGestureRecognizer(longPressGesture)
     }
     
     required init?(coder: NSCoder) {
@@ -35,7 +43,26 @@ class CardPokemonCell: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        containerView.pin.all()
         pokemonImageView.pin.all(12)
     }
     
+}
+
+// MARK: - Extensions
+
+extension CardPokemonCell: UIGestureRecognizerDelegate {
+    @objc func cellLongPressed(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            UIView.animate(withDuration: 0.1) {
+                self.containerView.backgroundColor = .systemGray6
+                self.containerView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.97, 0.97)
+            }
+            return
+        }
+        UIView.animate(withDuration: 0.1) {
+            self.containerView.backgroundColor = .clear
+            self.containerView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+        }
+    }
 }
